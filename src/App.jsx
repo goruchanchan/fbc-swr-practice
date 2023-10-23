a6bdcbcc5468659bc6ffe8481363003607e82e5f
goruchan <ppcfr481@yahoo.co.jp>
2023-10-23 22:08:43 +0900

    clean コード整理

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,12 @@ import "./App.css";
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
-
   const fetcher = () => fetch(url, { headers }).then((res) => res.json());
-
   const { data, error, isLoading } = useSWR(url, fetcher);
 
-  if (error)
-    return (
-      <>
-        <p>URL: {url}</p>
-        <p>Status: failed to load</p>
-      </>
-    );
-  if (isLoading)
-    return (
-      <>
-        <p>URL: {url}</p>
-        <p>Status: loading...</p>
-      </>
-    );
-
-  return (
-    <>
-      <p>URL: {url}</p>
-      <p>Status: {data.description}</p>
-    </>
-  );
+  if (error) return <p>Status: failed to load</p>;
+  if (isLoading) return <p>Status: loading...</p>;
+  return <p>Status: {data.description}</p>;
 }
 
 export default App;
